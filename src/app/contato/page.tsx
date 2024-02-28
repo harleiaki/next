e2dde1b55a0809a0e5c60e0a0c7d0e354f579722
Harlei Akira <harlei.akira@gmail.com>
2024-02-28 16:03:58 -0300

    router

---
+++ src/app/contato/page.tsx
@@ -0,0 +1,9 @@
+// app/sobre/page.tsx
+export default function ContatoPage() {
+    return (
+      <main>
+        <h1>Contato</h1>
+      </main>
+    );
+  }
+  
---
+++ src/app/sobre/page.tsx
@@ -0,0 +1,15 @@
+// app/sobre/page.tsx
+export const metadata = {
+    title: 'Sobre',
+    description: 'pagina sobre',
+  };
+  
+
+export default function SobrePage() {
+    return (
+      <main>
+        <h1>Sobre</h1>
+      </main>
+    );
+  }
+  
