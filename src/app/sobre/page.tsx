22b75f401764790dcd8f9160b694197ccd69e6de
Harlei Akira <harlei.akira@gmail.com>
2024-02-28 16:40:27 -0300

    IMC

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -1,15 +1,10 @@
-// app/sobre/page.tsx
-export const metadata = {
-    title: 'Sobre',
-    description: 'pagina sobre',
-  };
-  
-
 export default function SobrePage() {
-    return (
-      <main>
-        <h1>Sobre</h1>
-      </main>
-    );
-  }
-  
+  return (
+    <main>
+      <h1>Sobre</h1>
+      <h2 style={{ margin: '1600px 0' }} id="empresa">
+        A Empresa
+      </h2>
+    </main>
+  );
+}
--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
+
 export default function Menu() {
     return (
         <ul className="menu">
-            <li>Home</li>
-            <li>Sobre</li>
-            <li>Contato</li>
+            <li><Link href="/" prefetch={true}>Home</Link></li>
+            <li><Link href="/sobre#empresa" scroll={false} prefetch={true}>Sobre</Link></li>
+            <li><Link href="/contato" prefetch={true}>Contato</Link></li>
+            <li>
+                <Link href="/imc" prefetch={true}>IMC</Link>
+            </li>
         </ul>
     );
 }
